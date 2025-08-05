946fb5abcaac42635d7b18c0304c1360de6a3ee3
atiq <muhammadatiq757@gmail.com>
2025-08-05 12:08:03 +0600

    projec setup

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express from "express";
+import type { Request, Response } from "express";
+import cors from "cors";
+import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to Management system Backend",
+  });
+});
+
+// global error handler
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,63 @@
+// server fileimport { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+
+
+let server: Server;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://757mohammadatiq:757mohammadatiq@cluster0.iyiex3d.mongodb.net/tour-mangement-backend?retryWrites=true&w=majority&appName=Cluster0"
+    );
+
+    console.log("Connected to DB!!");
+    server = app.listen(5000, () => {
+      console.log("Server is listening to port 5000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+
+// 1. unhandled rejection error
+process.on("unhandledRejection", (err) => {
+  console.log("unhandled Rejection detected... server sutting down..", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// 2. uncaught rejection error
+process.on("uncaughtException", (err) => {
+  console.log(
+    "uncaught exceptional error detected... server sutting down..",
+    err
+  );
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// 3. signal termination (sigterm) error
+process.on("SIGTERM", () => {
+  console.log("SIGTERM Signal Rechived... server sutting down..");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
